196016c6b83f25f1cd9a039b6055fcb1bc20bad9
Viviane Alves <125809555+viviianealvesc@users.noreply.github.com>
2024-05-01 16:58:03 -0300

    feat: list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import Search from "./_components/search";
 import Header from "./_components/header";
+import CategoryList from "./_components/category-list";
 
 const Home = () => {
   return (
@@ -10,98 +10,15 @@ const Home = () => {
       <div className="px-5 pt-4">
         <Search />
       </div>
-=======
-import CategoryList from "./_components/category-list";
-import Header from "./_components/header";
-import Search from "./_components/search";
-import ProductList from "./_components/product-list";
-import { Button } from "./_components/ui/button";
-import { ChevronRightIcon } from "lucide-react";
-import { db } from "./_lib/prisma";
-import PromoBanner from "./_components/promo-banner";
-import RestaurantList from "./_components/restaurant-list";
-import Link from "next/link";
 
-const Home = async () => {
-  const products = await db.product.findMany({
-    where: {
-      discountPercentage: {
-        gt: 0,
-      },
-    },
-    take: 10,
-    include: {
-      restaurant: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
+     <div className="px-5 pt-6">
+       <CategoryList />
+     </div>
+      
 
-  return (
-    <>
-      <Header />
-      <div className="px-5 pt-6">
-        <Search />
-      </div>
-
-      <div className="px-5 pt-6">
-        <CategoryList />
-      </div>
-
-      <div className="px-5 pt-6">
-        <PromoBanner
-          src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizzas!"
-        />
-      </div>
-
-      <div className="space-y-4 pt-6">
-        <div className="flex items-center justify-between px-5">
-          <h2 className="font-semibold">Pedidos Recomendados</h2>
-
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-            asChild
-          >
-            <Link href="/products/recommended">
-              Ver todos
-              <ChevronRightIcon size={16} />
-            </Link>
-          </Button>
-        </div>
-        <ProductList products={products} />
-      </div>
-
-      <div className="px-5 pt-6">
-        <PromoBanner
-          src="/promo-banner-02.png"
-          alt="A partir de R$17,90 em lanches"
-        />
-      </div>
-
-      <div className="space-y-4 py-6">
-        <div className="flex items-center justify-between px-5">
-          <h2 className="font-semibold">Restaurantes Recomendados</h2>
-
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-            asChild
-          >
-            <Link href="/restaurants/recommended">
-              Ver todos
-              <ChevronRightIcon size={16} />
-            </Link>
-          </Button>
-        </div>
-        <RestaurantList />
-      </div>
->>>>>>> 36dcfb804cc828ef9b62df4113af7e9cb875692f
     </>
-  );
+
+   );
 };
 
 export default Home;
