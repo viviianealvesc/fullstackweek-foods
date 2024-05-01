fd902a33e499afbbdeb857afd8ce9d66d008e025
Viviane Alves <125809555+viviianealvesc@users.noreply.github.com>
2024-05-01 17:14:25 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Search from "./_components/search";
 import Header from "./_components/header";
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 
 const Home = () => {
@@ -14,10 +15,13 @@ const Home = () => {
      <div className="px-5 pt-6">
        <CategoryList />
      </div>
-      
 
-    </>
-
+     <div className="px-5 pt-6">
+         <Image src="/promo-banner-01.png" alt="Até 30% de descontos em pizzas" height={0} width={0}
+            className="h-auto w-full object-contain" sizes="100vw" quality={100} />
+     </div>
+    
+   </>
    );
 };
 
